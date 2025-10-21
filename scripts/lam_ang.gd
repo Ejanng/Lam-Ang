516c70fd111624f13bed4620348ba9a551b9194f
Earl Jasper Aquitania <earljasper21@gmail.com>
2025-10-21 20:09:00 +0800

    fix folder

---
+++ scripts/lam_ang.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+const WALK = 70.0
+const SPRINT = 140.0
+@onready var anim = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	
+	var current_speed = WALK
+	if Input.is_action_pressed("ui_select"):
+		current_speed = SPRINT
+	
+	# movements directions
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	
+	# animated sprites
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		velocity = direction * current_speed
+		move_and_slide()
+		
+		if abs(direction.x) > abs(direction.y):
+			anim.play("walk_side")
+			anim.flip_h = direction.x < 0 
+		else:
+			if direction.y < 0:
+				anim.play("walk_up")
+			else:
+				anim.play("walk_down")
+	else:
+		velocity = Vector2.ZERO
+		anim.play("idle")
