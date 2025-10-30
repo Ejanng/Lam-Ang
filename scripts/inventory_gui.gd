ce1a0d63c60ba5c27c3ece455ec82ed234029181
Earl Jasper Aquitania <earljasper21@gmail.com>
2025-10-30 13:23:03 +0800

    added artifacts gui

--- scripts/inventory_gui.gd
+++ scripts/inventory_gui.gd
@@ -6,7 +6,9 @@ signal closed
 var isOpen: bool = false
 
 @onready var inventory: Inventory = preload("res://Inventory/Item/playerInventory.tres")
+@onready var artifacts: Artifacts = preload("res://Inventory/Artifacts/playerArtifacts.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+@onready var artifact_slots: Array = $NinePatchRect2/GridContainer.get_children()
 
 func _ready() -> void:
 	update()
@@ -14,6 +16,9 @@ func _ready() -> void:
 func update():
 	for i in range(min(inventory.items.size(), slots.size())):
 		slots[i].update(inventory.items[i])
+		
+	#for i in range(min(artifacts.items.size(), artifact_slots.size())):
+		#artifact_slots[i].update(artifacts.items[i])
 
 func open():
 	visible = true
