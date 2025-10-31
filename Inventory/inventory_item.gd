1130731172c698945dd5cf3c2061d08bcf9883f0
Earl Jasper Aquitania <earljasper21@gmail.com>
2025-11-01 01:40:10 +0800

    fixing main

--- Inventory/inventory_item.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-class_name InventoryItem
-
-@export var name: String = ""
-@export var texture: Texture2D
-@export var scene: PackedScene
-@export var maxAmountPerStack: int = 10
