2a013f55c136c220d0985a88174692f64a9565b0
Earl Jasper Aquitania <earljasper21@gmail.com>
2025-10-31 09:28:11 +0800

    added values of artifacts

--- Inventory/inventory_item.gd
+++ Inventory/inventory_item.gd
@@ -5,8 +5,3 @@ class_name InventoryItem
 @export var name: String = ""
 @export var texture: Texture2D
 @export var scene: PackedScene
-@export var defStat: int
-@export var speedStat: float
-@export var healthStat: int
-@export var energyStat: int
-@export var critStat: float
